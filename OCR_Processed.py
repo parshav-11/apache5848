7f62fff76805eee015e9fd150c3082ed8a3bac45
Madhur Mittal <70881802+parshav-11@users.noreply.github.com>
2022-11-29 17:39:49 +0530

    Update OCR_Processed.py

--- OCR_Processed.py
+++ OCR_Processed.py
@@ -1,66 +1,33 @@
-import numpy as np
-import os
-import cv2
-import time
-import shutil
+import pytesseract
 from PIL import Image
-import matplotlib.pyplot as plt
-import keras_ocr
+import cv2
+import os
+from picamera import PiCamera
+from time import sleep
+camera = PiCamera()
+#camera = ["http://192.168.1.56:4747/capture"]
+camera.start_preview()
+sleep(1)
+while True:
+	camera.capture('/home/pi/Desktop/mukul.jpg')
+	camera.stop_preview()
+	img = cv2.imread('/home/pi/Desktop/mukul.jpg') #Open the image from which charectors has to be recognized
+	img = cv2.resize(img, (620,480) ) #resize the image if required
+	cv2.imshow('img', img)
+	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #convert to grey to reduce detials
+	gray = cv2.bilateralFilter(gray, 11, 17, 17) #Blur to reduce noise
 
-# keras-ocr will automatically download pretrained
-# weights for the detector and recognizer.
-pipeline = keras_ocr.pipeline.Pipeline()
+	original = pytesseract.image_to_string(gray)
+#test = (pytesseract.image_to_data(gray, lang=None, config='', nice=0) ) #get confidence level if required
+#print(pytesseract.image_to_boxes(gray))
+#print (original)
 
-image_frames = r'E:\PyPi Project\image_frames'
-def files():
-    try:
-        if not os.path.exists(image_frames):
-            os.makedirs(image_frames)
-    except OSError:
-        pass
-    #specify the source of video file
-    src_vid = cv2.VideoCapture(0)
-    return src_vid
 
-def process(src_vid):
-    #Use an index to integer-name the files
-    while src_vid.isOpened():
-        ret, frame = src_vid.read()
-        frame = frame[200:920,140:450]
-        gray_frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-        _, threshold = cv2.threshold(gray_frame, 115, 255, cv2.THRESH_BINARY)
-        time.sleep(1)
-        cv2.imshow('frame', threshold)
-        if not ret:
-            break
-        
-        index = 0
-        #name each frame and save an image
-        name = "./image_frames/frame" +str(index)+ ".png"
-        #save every 100th frame
-        if index % 1 == 0:
-            cv2.imwrite(name, threshold)
-            index += 1
-#         for i in os.listdir(image_frames):
-#             my_example = Image.open(r'C:\Users\madhu\image_frames\frame0.png')
-#             text = pytesseract.image_to_string(my_example)
-#             print(text)
-        #shutil.rmtree(image_frames)
-        images = [keras_ocr.tools.read(threshold) for threshold in [r'E:\PyPi Project\image_frames\frame0.png']]
-        prediction_groups = pipeline.recognize(images)
-        # Plot the predictions
-        fig, axs = plt.subplots(nrows=len(images), figsize=(20, 20))
-        for ax, image, predictions in zip(axs, images, prediction_groups):
-            keras_ocr.tools.drawAnnotations(image=image, predictions=predictions, ax=ax)
-        
-        if cv2.waitKey(10) & 0xFF == ord('q'):
-            break
-    src_vid.release()
-    cv2.destroyAllWindows()
-    
+	if os.path.exists("/home/pi/Desktop/mukul.jpg"):
+	  	os.remove("/home/pi/Desktop/mukul.jpg")
+	else:
+		print("The file does not exist")
 
-#main driver
-if __name__ == '__main__':
-#     files()
-    vid = files()
-    process(vid)
+	print (original)
+#print (test)
+
